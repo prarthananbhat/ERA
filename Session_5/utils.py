5fed1289927dc3a374f2f71b33d0ce8cc6e43db7
Prarthana Bhat <prarthananbhat@gmail.com>
2023-06-03 11:00:42 +0530

    updated utils with plot_accuracy metric

--- Session_5/utils.py
+++ Session_5/utils.py
@@ -11,4 +11,17 @@ def plot_samples(batch_data,batch_label):
     plt.xticks([])
     plt.yticks([])
   plt.show()
+
+def plot_accuracy_metrics(train_losses,train_acc,test_losses,test_acc):
+
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
+  plt.show()
   
